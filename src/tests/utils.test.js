2bc025106d2d365d7da0175017add9fdebd42c21
Bhupendra Singh <bhupendrasingh@Bhupendras-MacBook-Pro-3.local>
2024-10-08 21:01:58 +0530

    Fix function for custom delimiter

--- src/tests/utils.test.js
+++ src/tests/utils.test.js
@@ -15,8 +15,11 @@ describe('calculateSum Function', () => {
   test('adds "1, \n" to equal 1', () => {
     expect(calculateSum('1, \n')).toBe(1);
   });
-  test('adds "//;\\n1;2;3;" to equal to 6', () => {
+  test('adds "//;\n1;2;3" to equal to 6', () => {
     expect(calculateSum('//;\\n1;2;3;')).toBe(6);
+  });
+  test('adds "//**\n1;2;3" to equal to 6', () => {
+    expect(calculateSum('//**\\n1**2**3**')).toBe(6);
   });
 });
 
@@ -26,7 +29,7 @@ describe('sanitizeInput Function', () => {
     expect(sanitizeInput('s')).toBe(0);
   });
 
-  test('should return 0 if "1" is passed', () => {
+  test('should return 1 if "1" is passed', () => {
     expect(sanitizeInput('1')).toBe(1);
   });
 });
--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,25 @@
 export const calculateSum = (inputString) => {
-  const integerString = inputString.split(',');
-  if(integerString.length === 0) return 0;
-  return integerString.reduce((sum, current) => sum + sanitizeInput(current), 0);
+  let delimiter = ',';
+  let numbers = inputString;
+
+  // Check for custom delimiter
+  if (inputString.startsWith('//')) {
+    const delimiterEnd = inputString.indexOf('\\n');
+    if (delimiterEnd !== -1) {
+      delimiter = inputString.substring(2, delimiterEnd);
+      numbers = inputString.substring(delimiterEnd + 2);  // +2 to skip '\\n'
+    }
+  }
+
+  const integerStrings = numbers.split(delimiter);
+
+  if (integerStrings.length === 0) return 0;
+
+  return integerStrings.reduce((sum, current) => sum + sanitizeInput(current.trim()), 0);
 }
 
 export const sanitizeInput = (value) => {
-  if (!Number.isInteger(parseInt(value)) || value < 0) return 0;
-  return parseInt(value);
+  const parsedValue = parseInt(value, 10);
+  if (!Number.isInteger(parsedValue) || parsedValue < 0) return 0;
+  return parsedValue;
 }
